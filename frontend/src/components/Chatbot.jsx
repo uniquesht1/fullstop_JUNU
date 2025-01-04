91e6c43a9a9e87b89e5b3f089fb5ad9fa28e16fc
Sweta <swetasuwal001@gmail.com>
2025-01-04 08:01:28 +0545

    change search icon

--- frontend/src/components/Chatbot.jsx
+++ frontend/src/components/Chatbot.jsx
@@ -1,13 +1,14 @@
 
 
 import React from 'react'
+import { IoSearchSharp } from "react-icons/io5";
 
 const ChatbotUI = () => {
   return (
     <div>
       <div className="flex h-screen bg-gray-100 text-gray-900">
       {/* Main Content */}
-      <div className="flex flex-col flex-1  items-center">
+      <div className="flex flex-col flex-1  h-full items-center ">
 
         
         <div className="flex items-center justify-between w-full px-4 py-2 flex-col h-5/6">
@@ -43,22 +44,10 @@ const ChatbotUI = () => {
             />
             <button
               type="button"
-              className="ml-2 p-2 bg-blue-600 text-white rounded-full hover:bg-blue-500"
+              className="ml-2 p-2 bg-[#6abbee] text-white rounded-full hover:bg-blue-500"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-4.35-4.35M15 11a4 4 0 10-8 0 4 4 0 008 0z"
-                />
-              </svg>
+             <IoSearchSharp />
+                
             </button>
           </div>
         </div>
